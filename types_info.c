dafcad7a572b93ec48f24183f2f47a8fa682a2ec
MaxM2357 <33008868+MaxM2357@users.noreply.github.com>
2017-10-22 22:53:22 +0200

    Update types_info.c

--- types_info.c
+++ types_info.c
@@ -30,7 +30,6 @@ int main() {
     printf("short: %lu\n", sizeof(short));
     printf("int: %lu\n", sizeof(int));
     printf("long: %lu\n", sizeof(long));
-    printf("long: %lu\n", sizeof(long));
     printf("float: %lu\n", sizeof(float));
     printf("double: %lu\n", sizeof(double));
     printf("long double: %lu\n", sizeof(long double));
